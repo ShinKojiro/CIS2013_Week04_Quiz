5d902e8d21ae59c7f9fd8f23f5f36b00e1f3cc50
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-13 19:18:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,51 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//     for(int i = 1; i <= 100; i++){
+//         if(i % 3 == 0 && i % 5 == 0){
+//             cout << "FizzBuzz" << endl;
+//         }
+//         else if(i % 3 == 0){
+//             cout << "Fizz" << endl;
+//         }
+//         else if(i % 5 == 0){
+//             cout << "Buzz" << endl;
+//         }
+//         else{
+//             cout << i << endl;
+//         }
+//     }
+// }
+
 #include <iostream>
 
 using namespace std;
 
+void fizz(){
+    cout << "FIZZ";
+}
+
+void buzz(){
+    cout << "BUZZ";
+}
+
 int main(){
     for(int i = 1; i <= 100; i++){
         if(i % 3 == 0 && i % 5 == 0){
-            cout << "FizzBuzz" << endl;
+            fizz();
+            buzz();
         }
         else if(i % 3 == 0){
-            cout << "Fizz" << endl;
+            fizz();
         }
         else if(i % 5 == 0){
-            cout << "Buzz" << endl;
+            buzz();
         }
         else{
-            cout << i << endl;
+            cout << i;
         }
+        cout  << endl;
     }
 }
